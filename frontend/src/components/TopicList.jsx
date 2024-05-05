6ff9d5279e507d1f319cc85ceaee5eda7560261c
Rouslan Lakhno <rouslanlakhno@gmail.com>
2024-05-05 18:10:28 -0400

    Refactor with Mock Data and populate the photos and topics

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,30 +2,13 @@ import React from "react";
 
 import "../styles/TopicList.scss";
 import TopicListItem from './TopicListItem';
+import topics from '../mocks/topics';
 
-
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
 
 const TopicList = () => {
   return (
     <div className="top-nav-bar__topic-list">
-      {sampleDataForTopicList.map((item) => (
+      {topics.map((item) => (
         <TopicListItem
           key={`topic-${item.id}`}
           id={item.id}
