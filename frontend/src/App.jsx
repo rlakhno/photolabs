13acdb93fc05aee9ff4d0ab322f7ca1cea86f3fc
Rouslan Lakhno <rouslanlakhno@gmail.com>
2024-05-05 17:56:59 -0400

    Create the Home Page Layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,19 @@
 import React from 'react';
 
-import TopNavigationBar from './components/TopNavigationBar';
-import PhotoList from './components/PhotoList';
+// import TopNavigationBar from './components/TopNavigationBar';
+import HomeRoute from './routes/HomeRoute';
 
 import './App.scss';
 
 
 
-// Define the number of photos to render
-const numPhotos = 3;
-
-// Create an array of length numPhotos (e.g., [0, 1, 2] for numPhotos = 3)
-// const photos = Array.from({ length: numPhotos }, (_, index) => index);
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <TopNavigationBar />
 
-      <PhotoList />
+      <HomeRoute />
     </div>
 
   );
